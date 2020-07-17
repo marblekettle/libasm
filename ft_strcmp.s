2d11625187ca8fc49995d9f9a82a18b9dbf01add
marblekettle <tencivanni@gmail.com>
2020-07-17 11:26:17 +0200

    Tweaking ft_strcmp

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -2,7 +2,31 @@ section .text
     global _ft_strcmp
 
 _ft_strcmp:
+	mov rax, 0
     mov rcx, 0
     jmp loop
 
 loop:
+	mov rdx, [rdi + rcx]
+	cmp rdx, [rsi + rcx]
+	jg	plus
+	jl	minus
+	cmp	rdx, 0
+	je	equal
+
+equal:
+	mov rax, 0
+	ret
+
+plus:
+	mov rax, 1
+	ret
+
+minus:
+	mov rax, -1
+	ret
+
+
+4 and 0
+100 and 000 = 000
+100 and 001 = 000
